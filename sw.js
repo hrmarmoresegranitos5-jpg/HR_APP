18cbdbc8d0ca28efeabdce6fc33878cd625b593a
hrmarmoresegranitos5-jpg <hrmarmoresegranitos5@gmail.com>
2026-04-25 11:23:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,15 @@
-// HR Mármores e Granitos - Service Worker
-const CACHE = 'hr-marmores-v1';
+const CACHE = 'hr-v3';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
 self.addEventListener('install', function(e){
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache){
-      return cache.addAll(FILES);
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(function(cache){return cache.addAll(FILES);}));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', function(e){
-  e.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-    })
-  );
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(cached){
-      return cached || fetch(e.request).catch(function(){return caches.match('./index.html');});
-    })
-  );
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
 });
