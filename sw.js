49fcdd66a63fa73329808a466aba1f7adf15ba70
hrmarmoresegranitos5-jpg <hrmarmoresegranitos5@gmail.com>
2026-04-27 10:11:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hr-v3';
+const CACHE = 'hr-v4';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(e){
   e.waitUntil(caches.open(CACHE).then(function(cache){return cache.addAll(FILES);}));
